16068db5408cfc902f3e21424842d8ba231f22f7
Vương Khánh Nhân <vuongkhanhnhan@gmail.com>
2026-04-07 15:52:29 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,80 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import BoxColor from "./components/BoxColor";
+import Counter from "./components/Counter";
+import {
+  NavLink,
+  Routes,
+  Route,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 import TodoFeature from "./features/Todo/pages";
 
+function RedirectPost() {
+  const { postId } = useParams();
+  return <Navigate to={`/posts/${postId}`} replace />;
+}
+
 function App() {
-  const [isDark, setIsDark] = useState(false);
-
-  useEffect(() => {
-    if (isDark) {
-      document.body.classList.add("dark");
-    } else {
-      document.body.classList.remove("dark");
-    }
-  }, [isDark]);
-
   return (
-    <div className={isDark ? "App dark" : "App"}>
-      <div className="theme-toggle">
-        <button onClick={() => setIsDark(true)}>🌙 Dark</button>
-        <button onClick={() => setIsDark(false)}>☀️ Light</button>
-      </div>
+    <div className="App">
+      <nav className="navbar">
+        <h2 className="logo">My App</h2>
 
-      <TodoFeature />
+        <div className="menu">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/todos"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Todo
+          </NavLink>
+
+          <NavLink
+            to="/boxcolor"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Box
+          </NavLink>
+
+          <NavLink
+            to="/counter"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Counter
+          </NavLink>
+        </div>
+      </nav>
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<TodoFeature />} />
+
+          <Route path="/todos" element={<TodoFeature />} />
+          <Route path="/boxcolor" element={<BoxColor />} />
+          <Route path="/counter" element={<Counter />} />
+
+          <Route path="/post-list/:postId" element={<RedirectPost />} />
+
+          <Route path="/posts/:postId" element={<h2>Post Detail Page</h2>} />
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </div>
   );
 }
